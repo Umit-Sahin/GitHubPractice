12170a130b5c951e439b19d5f65c4a46e0a47219
Bilgin <bilginkalmis@gmail.com>
2022-03-14 22:11:10 +0100

    User2 third commit

--- src/test/java/team/GitHub/CheckPage2.java
+++ src/test/java/team/GitHub/CheckPage2.java
@@ -9,4 +9,12 @@ public class CheckPage2 {
         /* All team work on same page, same time, someone push his code after others merge. */
 
 
+
+
+
+    @Test
+    public void test2(){
+        System.out.println("Scenario 3 executing"); }
+
+
 }
