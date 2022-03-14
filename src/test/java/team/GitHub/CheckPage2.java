f0dc10a7b479cd7477e8093c5a239b36dcb6f547
coezen73 <85904903+coezen73@users.noreply.github.com>
2022-03-14 22:08:36 +0100

    User3 third commit

--- src/test/java/team/GitHub/CheckPage2.java
+++ src/test/java/team/GitHub/CheckPage2.java
@@ -9,4 +9,18 @@ public class CheckPage2 {
         /* All team work on same page, same time, someone push his code after others merge. */
 
 
+
+
+
+
+
+
+
+     @Test
+     public void test3(){
+     System.out.println("Scenario 3 executing"); }
+
+
+
+
 }
