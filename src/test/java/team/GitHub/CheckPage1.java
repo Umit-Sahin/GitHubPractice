f843a7441ab71aeee760db1616c380ff956b245b
Eric-Tokdada <erictokdada@gmail.com>
2022-03-14 23:20:54 +0300

    User1 first commit

--- src/test/java/team/GitHub/CheckPage1.java
+++ src/test/java/team/GitHub/CheckPage1.java
@@ -4,8 +4,9 @@ public class CheckPage1 {
     public static void main(String[] args) {
       /* All team work on same page, same time but different lines. */
 
-
-
+        String name1 = "User1";
+        String job1 = "QA Engineer";
+        System.out.println("It’s me.User 1");
 
 
 
