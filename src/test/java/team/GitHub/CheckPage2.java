8aa4f607732995ffb8efdbf29d0f2118864816b1
Eric-Tokdada <erictokdada@gmail.com>
2022-03-15 00:09:16 +0300

    User1 third commit

--- src/test/java/team/GitHub/CheckPage2.java
+++ src/test/java/team/GitHub/CheckPage2.java
@@ -8,5 +8,7 @@ public class CheckPage2 {
 
         /* All team work on same page, same time, someone push his code after others merge. */
 
-
+   @Test
+   public void test1(){
+       System.out.println("Scenario 3 executing"); }
 }
