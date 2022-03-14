1f75a59bafe15dfcf759e6aa77ed9d8d003ce0c0
coezen73 <85904903+coezen73@users.noreply.github.com>
2022-03-14 21:46:59 +0100

    User3 second commit

--- src/test/java/team/GitHub/CheckPage1.java
+++ src/test/java/team/GitHub/CheckPage1.java
@@ -4,12 +4,9 @@ public class CheckPage1 {
     public static void main(String[] args) {
       /* All team work on same page, same time but different lines. */
 
-        String name1 = "User1";
-        String job1 = "QA Engineer";
-        System.out.println("It’s me.User 1");
-
-
-
+    String name1 = "User1";
+    String job1 = "QA Engineer";
+    System.out.println("It’s me.User 1");
 
     String name2 = "User2";
     String job2 = "QA Engineer";
@@ -30,6 +27,16 @@ public class CheckPage1 {
     boolean input=true;
 
 
+
+
+
+
+
+
+
+
+    System.out.println("User3 working.");
+    System.out.println("Test 3 done.");
 
 
 }
